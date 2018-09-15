71a30285b255f0dc7b4627eda9c83a72c300ecce
kwebi <kwebi@foxmail.com>
2018-09-15 18:54:54 +0800

    copy

--- leetcode/src/Lc731.java
+++ leetcode/src/Lc731.java
@@ -1,35 +1,77 @@
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class Lc731 {
-    private HashMap hashMap = new HashMap();
+    private static List<int[]> singleBlockedTimes;
+    private static List<int[]> doubleBlockedTimes;
     public Lc731() {
-
+        singleBlockedTimes = new ArrayList();
+        doubleBlockedTimes = new ArrayList();
     }
-    public boolean book(int start, int end) {
-        boolean ans = true;
-        if (hashMap.isEmpty()) {
-            for (int i = start; i < end; i++) {
-                hashMap.put(i,0);
-            }
-        } else {
-            for (int i = start; i < end; i++) {
-                if (!hashMap.containsKey(i)) {
-                    hashMap.put(i,0);
-                } else {
-                    if ((int)hashMap.get(i) >= 2) {
-                        ans = false;
-                        break;
-                    }
-                }
+    private static void addAndUpdate(int[] input, List<int[]> singleBlockedTimes, List<int[]> doubleBlockedTimes) {
+        // pick our intervals that overlaps with input
+        List<int[]> conflicts = new ArrayList();
+        for (int[] interval : singleBlockedTimes) {
+            if (interval[1] <= input[0] || interval[0] >= input[1]) {
+                continue;
+            } else {
+                conflicts.add(interval);
             }
         }
-        if (ans) {
-            for (int i = start; i < end; i++) {
-                int v = (int)hashMap.get(i);
-                hashMap.replace(i, v+1);
+
+        // no conflict, no work to do
+        if (conflicts.size() == 0) {
+            singleBlockedTimes.add(input);
+            return;
+        }
+
+        // merge and update both lists
+        List<int[]> allTimes = new ArrayList<>(conflicts);
+        allTimes.add(input);
+        Collections.sort(allTimes, new Comparator<int[]>() {
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+        int[] pre = allTimes.get(0);
+        for (int i = 1; i < allTimes.size(); i++) {
+            int[] cur = allTimes.get(i);
+            if (cur[0] >= pre[1]) {
+                pre = cur;
+                continue;
+            } else if (cur[1] > pre[1]) {
+                singleBlockedTimes.add(new int[]{pre[0], cur[0]});
+                doubleBlockedTimes.add(new int[]{cur[0], pre[1]});
+                pre = new int[]{pre[1], cur[1]};
+            } else {
+                singleBlockedTimes.add(new int[]{pre[0], cur[0]});
+                doubleBlockedTimes.add(new int[]{cur[0], cur[1]});
+                //singleBlockedTimes.add(new int[]{cur[1], pre[1]});
+                pre = new int[]{cur[1], pre[1]};
             }
         }
-        return ans;
+        singleBlockedTimes.add(pre);
+
+        for (int[] interval : conflicts) {
+            singleBlockedTimes.remove(interval);
+        }
+    }
+
+    public static boolean book(int start, int end) {
+        for (int i = 0; i < doubleBlockedTimes.size(); i++) {
+            int[] cur = doubleBlockedTimes.get(i);
+
+            if (cur[1] <= start || cur[0] >= end) {
+                continue;
+            } else {
+                return false;
+            }
+        }
+        addAndUpdate(new int[]{start, end}, singleBlockedTimes, doubleBlockedTimes);
+        return true;
     }
     public static void main(String[] args) {
         Lc731 o = new Lc731();
