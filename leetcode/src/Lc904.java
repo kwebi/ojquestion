f7b0f4d27e18a1e630912da7ad22f4fdf24ba117
kwebi <kwebi@foxmail.com>
2018-09-22 15:00:31 +0800

    0101... tle

--- leetcode/src/Lc904.java
+++ leetcode/src/Lc904.java
@@ -4,16 +4,23 @@ import java.util.List;
 public class Lc904 {
     public int totalFruit(int[] tree) {
         int m = tree.length;
+        int[] dp = new int[m];
         int ans = 0;
         for (int i = 0; i < m; i++) {
+            if (dp[i] > 1) {
+                continue;
+            }
             int tmp = 0;
             List<Integer> type = new ArrayList<>(2);
             type.add(tree[i]);
             tmp++;
-            for (int j = i; j < m; j++) {
+            int j;
+            int diff=m;
+            for (j = i; j < m; j++) {
                 if (!type.contains(tree[j]) && type.size() < 2) {
                     type.add(tree[j]);
                     tmp++;
+                    diff = j;
                 } else if (type.contains(tree[j])) {
                     if (j == i) continue;
                     tmp++;
@@ -24,10 +31,13 @@ public class Lc904 {
             if (ans <= tmp) {
                 ans = tmp;
             }
+            for (int k=i;k<diff;k++) {
+                dp[k] = ans-(k-i);
+            }
         }
         return ans;
     }
     public static void main(String[] args) {
-        System.out.println(new Lc904().totalFruit(new int[] {1,2,1}));
+        System.out.println(new Lc904().totalFruit(new int[] {0,0,0,0,0,0,0,0,0,0}));
     }
 }
