f2126d37e8a59664c4068197d50df34a32e94d52
kwebi <1565718281@qq.com>
2018-09-30 18:59:17 +0800

    short version

--- leetcode/C++/7.cpp
+++ leetcode/C++/7.cpp
@@ -9,67 +9,18 @@ class Solution
   public:
     int reverse(int x)
     {
-        int z = 1;
-        long long tmp = x;
-        if (x < 0)
+        int rev = 0;
+        while (x != 0)
         {
-            z = -1;
-            tmp = -tmp;
+            int pop = x % 10;
+            x /= 10;
+            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && pop > 7))
+                return 0;
+            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && pop < -8))
+                return 0;
+            rev = rev * 10 + pop;
         }
-
-        vector<int> vec;
-        int i = 0;
-        
-        while (tmp >= 1)
-        {
-            vec.push_back(tmp % 10);
-            tmp /= 10;
-            ++i;
-        }
-
-        int zero_num = 0;
-        for (int j = 0; j < vec.size(); ++j)
-        {
-            if (vec[j] == 0)
-            {
-                ++zero_num;
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        if (vec.size() - zero_num > 10)
-        {
-            return 0;
-        }
-
-        if (i == 1)
-        {
-            return x;
-        }
-
-        long long sum = 0;
-        for (int j = zero_num; j < vec.size(); ++j)
-        {
-            sum += vec[j] * (static_cast<long long>(pow(10LL, vec.size() - j - 1)));
-            if (z == -1)
-            {
-                if (-sum < INT_MIN)
-                {
-                    return 0;
-                }
-            }
-            else
-            {
-                if (sum > INT_MAX)
-                {
-                    return 0;
-                }
-            }
-        }
-        return static_cast<int>(sum * z);
+        return rev;
     }
 };
 
