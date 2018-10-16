38a1d0a2b90349f45c22340233b17de3e701d263
kwebi <1565718281@qq.com>
2018-10-16 21:21:31 +0800

    i & (i-1) 比特位计数

--- leetcode/C++/338.cpp
+++ leetcode/C++/338.cpp
@@ -10,7 +10,7 @@ class Solution
         res[0] = 0;
         for (int i = 1; i <= num; ++i)
         {
-            res[i] = res[i >> 1] + (i & 1);
+            res[i] = res[i & (i - 1)] + 1;
         }
         return res;
     }
