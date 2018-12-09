54a323ec886fc0d41db660f24bf220a3c9a1c6e8
kwebi <1565718281@qq.com>
2018-12-09 12:20:41 +0800

    198

--- leetcode/C++/198.cpp
+++ leetcode/C++/198.cpp
@@ -7,26 +7,14 @@ class Solution
   public:
     int rob(vector<int> &nums)
     {
-        int len = nums.size();
-        if (len == 0)
+        int a = 0, b = 0;
+        for (int i = 0; i < nums.size(); ++i)
         {
-            return 0;
+            int m = a, n = b;
+            a = n + nums[i];
+            b = maxNum(m, n);
         }
-        if (len == 1)
-        {
-            return nums[0];
-        }
-        int a = 0;
-        int b = 0;
-        a = nums[0];
-        b = nums[0] > nums[1] ? nums[0] : nums[1];
-        for (int i = 2; i < len; ++i)
-        {
-            int tmp = b;
-            b = maxNum(b, a + nums[i]);
-            a = tmp;
-        }
-        return b;
+        return maxNum(a, b);
     }
     int maxNum(int a, int b)
     {
---
+++ leetcode/Go/198.go
@@ -0,0 +1,20 @@
+package main
+
+func rob(nums []int) int {
+	a, b := 0, 0
+	for i := 0; i < len(nums); i++ {
+		m, n := a, b
+		a = n + nums[i]
+		b = maxInt(m, n)
+	}
+	return maxInt(a, b)
+
+}
+
+func maxInt(a int, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
