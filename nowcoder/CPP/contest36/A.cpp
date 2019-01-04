e138aaea719c299802251664f696d4563e5e669c
kwebi <1565718281@qq.com>
2019-01-04 22:18:14 +0800

    优化

--- nowcoder/CPP/contest36/A.cpp
+++ nowcoder/CPP/contest36/A.cpp
@@ -5,40 +5,6 @@ using namespace std;
 char s[100000];
 bool flag = false;
 
-void compare(int j, int n)
-{
-    int k = j;
-    for (int i = 0; i < n; i++)
-    {
-        int tmp;
-        if (j > n - k + n)
-        {
-            break;
-        }
-        else if (j >= n)
-        {
-            tmp = j - n;
-        }
-        else
-        {
-            tmp = j;
-        }
-        if (s[tmp] < s[i])
-        {
-            flag = true;
-            break;
-        }
-        else if (s[tmp] > s[i])
-        {
-            break;
-        }
-        else
-        {
-            j++;
-        }
-    }
-}
-
 int main(int argc, char const *argv[])
 {
     int n;
@@ -46,9 +12,15 @@ int main(int argc, char const *argv[])
     scanf("%d", &n);
     scanf("%s", s);
 
-    for (int j = 1; j < n; j++)
+    int len = 0;
+    for (int j = 1; j < n && !flag; j++)
     {
-        compare(j, n);
+        if (s[j] < s[len])
+            flag = true;
+        else if (s[j] == s[len])
+            len++;
+        else
+            len = 0;
     }
     if (flag)
         printf("YES\n");
