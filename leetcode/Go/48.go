0efc92852f8ad98acf8a20e075b856040e8ea453
kwebi <1565718281@qq.com>
2019-02-21 12:14:20 +0800

    旋转数组90度

---
+++ leetcode/Go/48.go
@@ -0,0 +1,18 @@
+package main
+
+func rotate(matrix [][]int)  {
+	for i :=0; i < len(matrix); i++ {
+		for j := i+1; j < len(matrix); j++ {
+			tmp := matrix[i][j]
+			 matrix[i][j]=matrix[j][i]
+			 matrix[j][i]=tmp
+		}
+	}
+	for i:=0;i<len(matrix);i++ {
+		for j:=0;j<len(matrix)/2;j++{
+			tmp := matrix[i][j]
+			matrix[i][j]=matrix[i][len(matrix)-1-j]
+			matrix[i][len(matrix)-1-j]=tmp
+		}
+	}
+}
