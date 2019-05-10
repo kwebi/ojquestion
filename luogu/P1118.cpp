94a1dad882bfcdffdd3043e6666f9b5327e17023
kwebi <1565718281@qq.com>
2019-05-10 22:36:33 +0800

    dfs优化

--- luogu/P1118.cpp
+++ luogu/P1118.cpp
@@ -1,9 +1,15 @@
+#include <vector>
+#include <cstring>
 #include <stdio.h>
 #include <algorithm>
 
 using namespace std;
 
 int a[13];
+int n, sum;
+bool vis[13];
+bool flag = false;
+vector<int> res;
 
 int C(int u, int d)
 {
@@ -18,30 +24,42 @@ int C(int u, int d)
     return a / b;
 }
 
+void dfs(int dep, int s)
+{
+    if (flag)
+        return;
+    if (dep > n || s > sum)
+        return;
+    if (dep == n)
+    {
+        if (s == sum)
+        {
+            flag = true;
+            for (int i = 0; i < res.size(); ++i)
+                printf("%d ", res[i]);
+        }
+        return;
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        if (!vis[i])
+        {
+            vis[i] = true;
+            int p = C(dep, n - 1);
+            res.push_back(a[i]);
+            dfs(dep + 1, s + a[i] * p);
+            res.pop_back();
+            vis[i] = false;
+        }
+    }
+}
+
 int main()
 {
-    int n, sum;
+    memset(vis, 0, sizeof(vis));
     scanf("%d%d", &n, &sum);
     for (int i = 0; i < n; i++)
         a[i] = i + 1;
-    do
-    {
-        int r = 0;
-        for (int i = 0; i < n; i++)
-        {
-            r += a[i] * C(i, n - 1);
-            if (r > sum)
-                sort(a + i + 1, a + n, [](int x, int y) { return x > y; });
-        }
-        if (r == sum)
-        {
-            for (int i = 0; i < n; i++)
-            {
-                printf("%d ", a[i]);
-            }
-            break;
-        }
-    } while (next_permutation(a, a + n));
-
+    dfs(0, 0);
     return 0;
 }
